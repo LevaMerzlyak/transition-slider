46496b1e779e4f17c0ce612e8cef5ecce5a8c3cf
LevaMerzlyak <13dir13@gmail.com>
2017-10-12 11:11:57 +0300

    change slider init function

--- js/SliderInit.js
+++ js/SliderInit.js
@@ -3,31 +3,32 @@ function SliderInit(sSelector) {
   var i = this;
 
   i.objects = $(sSelector);
-  i.wrap = i.objects.find('.objectSlider__item');
-  i.img = i.wrap.find('.slide__img');
+  i.object = i.objects.find('.objectSlider__item');
+  i.wrap = i.object.find('.slide__bg')
+  i.img = i.object.find('.slide__img');
   i.prev = i.objects.find('.slideNav__btn_prev');
   i.next = i.objects.find('.slideNav__btn_next');
   i.slide = i.objects.find('.slide__head');
 
   i.cover = function () {
-   $(i.wrap).each(function () {
+   $(i.object).each(function () {
 
-    var img = $(this).find('.slide__img'),
-     src = img.attr('src');
+    var img = $(this).find('.slide__img')
+        ,src = img.attr('src')
+        ,wrap = $(this).find('.slide__bg')
+        ;
 
-    $(this).css({ 'background-image': 'url(' + src + ')'
-                  ,'background-size': 'cover',
-                  'background-position': 'center' } );
+    wrap.css({ 'background-image': 'url(' + src + ')' } );
 
    } );
 
-   i.img.hide();
+   //i.img.hide();
 
   };
   i.showSlide = function() {
     var slide = $(this);
     
-
+    slide.closest('.objectSlider__item').addClass('opened');
   }
 
 	//$(window).ready(i.cover);
